9e806e6b8f9a06c0bf1c3783296dcb532266d1cc
quildtide <42811940+quildtide@users.noreply.github.com>
2025-12-28 19:55:13 -0500

    Add pre-v1.12 alternative methods

--- src/random.jl
+++ src/random.jl
@@ -8,19 +8,32 @@ const DenseSignArrays = Union{
     Base.FastContiguousSubArray{UInt8,N,<:DenseArray{Sign}}
 } where {N}
 
-function rand!(rng::AbstractRNG, A::DenseSignArrays)
-    # UnsafeView creates a writeable view that treats A as a Bool array
-    # Allows writing the bits of rand!(Array{Bool}) into A without allocating new memory
-    GC.@preserve A rand!(rng, UnsafeView{Bool}(pointer(A), length(A)), SamplerType{Bool}())
-    return A
-end
+@static if VERSION >= v"1.12"
+    # This only works properly because of JuliaLang/julia#57101
+    # More complicated than the alternative version, but seems to be more canonical
+    function rand!(rng::AbstractRNG, A::DenseSignArrays)
+        # UnsafeView creates a writeable view that treats A as a Bool array
+        # Allows writing the bits of rand!(Array{Bool}) into A without allocating new memory
+        GC.@preserve A rand!(rng, UnsafeView{Bool}(pointer(A), length(A)), SamplerType{Bool}())
+        return A
+    end
 
-# The below method fixes dispatch from the non-mutating vector call to the mutating rand call
-rand!(rng::AbstractRNG, A::DenseSignArrays, sp::Sampler) = rand!(rng, A)
+    # The below method fixes dispatch from the non-mutating vector call to the mutating rand call
+    rand!(rng::AbstractRNG, A::DenseSignArrays, sp::Sampler) = rand!(rng, A)
 
-function rand(rng::AbstractRNG, ::SamplerType{Sign}, dim1::Integer, extra_dims::Integer...)
-    # Strange function signature to avoid method ambiguities
-    A = Array{Sign}(undef, Dims(dim1, extra_dims...))
-    rand!(rng, A)
-    return A
+    function rand(rng::AbstractRNG, ::SamplerType{Sign}, dim1::Integer, extra_dims::Integer...)
+        # Strange function signature to avoid method ambiguities
+        A = Array{Sign}(undef, Dims(dim1, extra_dims...))
+        rand!(rng, A)
+        return A
+    end
+else
+    function rand!(rng::AbstractRNG, A::DenseSignArrays)
+        # This method is sus, but seems to work with no noticeable overhead
+        rand!(rng, reinterpret(Bool, A))
+        return A
+    end
+    
+    # No noticeable overhead
+    rand(rng::AbstractRNG, ::SamplerType{Sign}, dim1::Integer, extra_dims::Integer...) = reinterpret(Sign, rand(rng, SamplerType(Bool), dim1, extra_dims...))
 end
